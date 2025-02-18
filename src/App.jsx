7c6a34b0a549f2557f3724b142d0c85791885a8a
jimbojones1 <jimmyhaff3@gmail.com>
2025-02-18 13:51:19 -0500

    added signup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// src/App.jsx
+
+import { Routes, Route } from 'react-router'
+
+import NavBar from './components/NavBar/NavBar';
+import SignUpForm from './components/SignUpForm/SignUpForm';
+
+const App = () => {
+  
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/sign-up' element={<SignUpForm />} />
+      </Routes> 
+    </>
+  );
+};
+
+export default App;
+
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+// src/components/NavBar/NavBar.jsx
+
+import { Link } from "react-router";
+
+const NavBar = () => {
+    return (
+      <nav>
+       <ul>
+        <li><Link to='/sign-up'>Sign up</Link></li>
+       </ul>
+      </nav>
+    );
+  };
+  
+  export default NavBar;
+  
+  
