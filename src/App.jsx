e827c069299c90d8eb77e72d28de50139c40603d
jimbojones1 <jimmyhaff3@gmail.com>
2025-02-19 12:03:25 -0500

    fixed user

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 // src/App.jsx
-
+import { useContext } from 'react';
 import { Routes, Route } from 'react-router'
-
 import NavBar from './components/NavBar/NavBar';
 import SignUpForm from './components/SignUpForm/SignUpForm';
 import SignInForm from './components/SignInForm/SignInForm';
 import Dashboard from './components/Dashboard/Dashboard';
+
+
+import { UserContext } from './contexts/UserContext';
 const App = () => {
-  
+ 
+  const { user } = useContext(UserContext)
+
   return (
     <>
       <NavBar />
